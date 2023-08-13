44d17e121dfea91c21c3ba25f6c2b0e314536971
ericcnguyen04 <ericc.win@gmail.com>
2023-08-13 15:22:24 -0400

    navbar working on it

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Home from './Components/Home'
 import Biology from './Components/Biology'
 import English from './Components/English';
 import UsHistory from './Components/US-history'
+import AmericanGov from './Components/American-Gov'
 
 function App() {
   return (
@@ -28,10 +29,17 @@ function App() {
             path="/english"
             element={<English />}
           />
+
           <Route
             path="/us-history"
             element={<UsHistory />}
           />
+
+          <Route 
+            path='/american-government'
+            element={<AmericanGov/>}
+          />
+
         </Routes>
       </div>
       
