1146230bcfbd4c40c6196f19c8be1bf1c7cad81f
ericcnguyen04 <ericc.win@gmail.com>
2023-08-12 22:47:45 -0400

    import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
 function App() {
   return (
     <div className="App">
