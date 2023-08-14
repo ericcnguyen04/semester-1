f327de38c0ff4f50af1d01359ced522180acc4c4
ericcnguyen04 <ericc.win@gmail.com>
2023-08-14 19:32:28 -0400

    adding style

--- src/Components/US-history.jsx
+++ src/Components/US-history.jsx
@@ -1,5 +1,8 @@
-import Navbar from "./partials/Navbar"
 import '../App.css'
+import 'bulma/css/bulma.min.css';
+
+import Navbar from "./partials/Navbar"
+
 export default function English () {
     return (
         <div>
@@ -7,6 +10,12 @@ export default function English () {
             <h1>
                 <a href='https://go.view.usg.edu/d2l/home/2928804' target="_blank">US history</a>
             </h1>
+            <span class="icon-text">
+                <span class="icon">
+                    <i class="fas fa-home"></i>
+                </span>
+                <span>Go to ecore</span>
+            </span>
             <div>
                 <h2>Unit 1 - Cultures Collide</h2>
                 <h3>interaction of natives and europeans and the consequences</h3>
