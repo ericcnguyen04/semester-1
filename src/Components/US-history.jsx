42bfa233b3c9d8bb4e9a90c8b3adbbbbd37420ff
ericcnguyen04 <ericc.win@gmail.com>
2023-08-14 19:35:55 -0400

    gamer time

--- src/Components/US-history.jsx
+++ src/Components/US-history.jsx
@@ -7,15 +7,10 @@ export default function English () {
     return (
         <div>
             <Navbar/>
-            <h1>
-                <a href='https://go.view.usg.edu/d2l/home/2928804' target="_blank">US history</a>
-            </h1>
-            <span class="icon-text">
-                <span class="icon">
-                    <i class="fas fa-home"></i>
-                </span>
-                <span>Go to ecore</span>
-            </span>
+            <button class="button">
+            <a href='https://go.view.usg.edu/d2l/home/2928804' target="_blank">US history ecore</a>
+            </button>
+
             <div>
                 <h2>Unit 1 - Cultures Collide</h2>
                 <h3>interaction of natives and europeans and the consequences</h3>
