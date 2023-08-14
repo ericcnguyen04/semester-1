a16159afe7f84765155584f10f4b642cd214eb68
ericcnguyen04 <ericc.win@gmail.com>
2023-08-14 12:42:10 -0400

    going to take notes

--- src/Components/US-history.jsx
+++ src/Components/US-history.jsx
@@ -2,10 +2,10 @@ export default function English () {
     return (
         <div>
             <h1>
-                US history
+                <a href='https://go.view.usg.edu/d2l/home/2928804' target="_blank">US history</a>
             </h1>
             <div>
-                
+                <h2>will</h2>
             </div>
         </div>
     )
