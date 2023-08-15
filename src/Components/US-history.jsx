b7b8121bd7dddca67e90e5b7b320539d2334d22b
ericcnguyen04 <ericc.win@gmail.com>
2023-08-15 14:25:29 -0400

    aye shade

--- src/Components/US-history.jsx
+++ src/Components/US-history.jsx
@@ -37,6 +37,20 @@ export default function English () {
                     <p>Agriculture</p>
                     <ul>
                         <li>Agriculture flourished in the fertile river valleys between the Mississippi River and the Atlantic Ocean,aka Eastern Woodlands. There, three crops in particular—corn, beans, and squash, aka Three Sisters—provided nutritional needs necessary to sustain cities and civilizations. </li>
+                        <li>Woodland areas from the Great Lakes and the Mississippi River to the Atlantic coast, Native communities managed their forest resources by burning underbrush to create vast parklike hunting grounds and to clear the ground for planting the Three Sisters.</li>
+                        <li>When crops hard to grow, they move elsewhere. (letting initial farm regrow)</li>
+                        <li>Analysis of remains reveals that societies transitioning to agriculture often experienced weaker bones and teeth.</li>
+                        <li>Since farmers made the most food, new skills were developed: soldiers, religious leader, artists.</li>
+                        <li>mothers often wielded enormous influence at local levels, and men’s identities and influence often depended on their relationships to women. better sexual and marital freedom than European culture.</li>
+                    </ul>
+
+                    <p>Chaco Canyon</p>
+                    <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
                     </ul>
             </div>
         </div>
