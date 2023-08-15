e6bf708bdf61ad3562cd38d4033452660f88ce68
ericcnguyen04 <ericc.win@gmail.com>
2023-08-15 19:28:45 -0400

    hyperfocusl

--- src/Components/US-history.jsx
+++ src/Components/US-history.jsx
@@ -44,8 +44,66 @@ export default function English () {
                         <li>mothers often wielded enormous influence at local levels, and men’s identities and influence often depended on their relationships to women. better sexual and marital freedom than European culture.</li>
                     </ul>
 
-                    <p>Chaco Canyon</p>
+                    <p>Chaco Canyon and slavery.. kinda</p>
                     <ul>
+                        <li>Chaco Canyon in northern New Mexico was home to ancestral Puebloan peoples between 900 and 1300 CE. As many as fifteen thousand individuals lived in the Chaco Canyon complex in present-day New Mexico.</li>
+                        <li>Sophisticated agricultural practices, extensive trading networks, and even the domestication of animals like turkeys allowed the population to swell.</li>
+                        <li>The Puebloan people of Chaco Canyon faced several ecological challenges, including deforestation and overirrigation, made smaller settlements</li>
+                        <li>Cahokia was politically organized around chiefdoms, a hierarchical, clan-based system that gave leaders both secular and sacred authority. The size of the city and the extent of its influence suggest that the city relied on a number of lesser chiefdoms under the authority of a paramount leader. </li>
+                        <li>War captives were enslaved, and these captives formed an important part of the economy in the North American Southeast. Native American slavery was not based on holding people as property. Instead, Native Americans understood the enslaved as people who lacked kinship networks. Slavery, then, was not always a permanent condition. Very often, a formerly enslaved person could become a fully integrated member of the community. Adoption or marriage could enable an enslaved person to enter a kinship network and join the community.</li>
+                    </ul>
+
+                    <h6>European Expansion</h6>
+                    <ul>
+                        <li>Scandinavian seafarers reached the New World long before Columbus. At their peak they sailed as far east as Constantinople and raided settlements as far south as North Africa. </li>
+                        <li><strong>European Renaissance and Expansion</strong> <br></br>
+                        Crusades linked Europe with Asian wealth, knowledge.Rediscovered/adopted Greek, Roman, Muslim knowledge.
+                        Hemispheric trade and knowledge dissemination sparked Renaissance.
+                        European expansion fueled by Asian goods, creating wealth.
+                        Competition among European nations for trade supremacy.
+                        </li>
+                        <li>European nation-states consolidated under the authority of powerful kings. A series of military conflicts between England and France—the Hundred Years’ War—accelerated nationalism and cultivated the financial and military administration necessary to maintain nation-states. </li>
+                        <li>Prince Henry the Navigator invested in exploration, technology.
+                        Portuguese sailors developed astrolabe (latitude calculation) and caravel (ocean-exploration ship).</li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+
+                    <h6>Spanish Exploration and Conquest</h6>
+                    <ul>
+                        <li>Spanish Conquest and New World Empire:</li>
+                        <div>
+                        News of Spanish conquest led to influx of Spaniards seeking wealth.
+                        Conquest spread from Caribbean foothold to New World empire.
+                        Motives: land, gold, titles; "serve God and the king" and get rich.
+                        Mercenaries joined, sought human/material wealth.
+                        </div>
+                        <li>Encomienda and Labor Exploitation:</li>
+                        <div>
+                        Encomienda: exploitative feudal arrangement tying Indigenous laborers to estates.
+                        Encomenderos brutalized laborers; Las Casas's account led to abolition.
+                        Repartimiento replaced encomienda, but still exploited Native population.
+                        </div>
+                        <li>Maya and Aztec Civilizations:</li>
+                        <div>
+                        Maya civilization with advanced features in Central America.
+                        Maya collapsed due to droughts, unsustainable practices.
+                        Rise of Aztecs: militaristic migrants built largest New World empire.
+                        Tenochtitlán: sprawling city on Lake Texcoco, impressive architecture.    
+                        </div>
+                        <li>Aztec Empire and Spanish Conquest:</li>
+                        <div>Aztecs ruled through tribute from subject peoples.
+                        Spanish conqueror Cortés invaded Mexico, aided by Native allies.
+                        Spanish captured Montezuma, looted gold and silver reserves.
+                        Aztec uprising, "night of sorrows," siege, and sack of Tenochtitlán.
+                        </div>
+                        <li>Incan Empire and Spanish Conquest:</li>
+                        <div>
+                        Quechuas (Incas) managed vast Andean empire.
+                        Empire stretched across western South America.
+                        Incan empire vulnerable due to unrest, smallpox epidemic.
+                        Spanish conqueror Pizarro took control of Incan empire, Cuzco.    
+                        </div>
                         <li></li>
                         <li></li>
                         <li></li>
