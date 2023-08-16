cee7378934b65dfa12d202a210e2a010b8d9fc10
ericcnguyen04 <ericc.win@gmail.com>
2023-08-16 09:22:27 -0400

    set up

--- src/Components/biology/Cancer.jsx
+++ src/Components/biology/Cancer.jsx
@@ -3,8 +3,22 @@ import 'bulma/css/bulma.min.css';
 
 export default function Cancer () {
     return (
-        <div>
-            cancerdd
+        <div className='content'>
+            <div>
+                <h1>Day 2</h1>
+                <h6></h6>
+                    <p>
+
+                    </p>
+                <h6>
+                    <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </h6>
+            </div>
         </div>
     )
 }
